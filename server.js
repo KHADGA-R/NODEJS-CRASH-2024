1f08f71a685488dda81b87b134c1f6e73ff21c1f
KHADGA-R <parajuli.kr@gmail.com>
2025-05-24 22:57:54 -0700

    Used fs module for routing

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import http from 'http';
 import dotenv from 'dotenv';
+import fs from 'fs/promises';
+import  url from 'url';
+import path from 'path';
 
 // Load environment variables from .env file
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+//Get Current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
     //res.setHeader('Content-Type', 'text/html');
     //res.statusCode = 404;
    // res.writeHead(500, {'Content-Type': 'application/json'})
@@ -15,16 +24,20 @@ const server = http.createServer((req, res) => {
     try{
         //Check if the request method is GET
         if (req.method === 'GET'){
+            let filePath;
             if (req.url === '/'){
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end('<h1>Homepage</h1>');
+                filePath = path.join(__dirname, 'public', 'index.html');
+                
             } else if (req.url === '/about'){
-                res.writeHead(200, {'Content-Type' : 'text/html'});
-                res.end('<h1>About</h1>');
+               filePath = path.join(__dirname, 'public', 'about.html');
             } else{
-                res.writeHead(404, {'Content-Type' : 'text/html'});
-                res.end(`<h1>Not Found</h1>`);
+                throw new Error('Not Found');  
             }
+            
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error('Method not allowed');
         }
