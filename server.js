6dbf0b0e7809ea171db1f47ce6591c91b2e6a344
KHADGA-R <parajuli.kr@gmail.com>
2025-05-24 17:43:58 -0700

    Fix server error

--- server.js
+++ server.js
@@ -4,16 +4,37 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
     //res.setHeader('Content-Type', 'text/html');
     //res.statusCode = 404;
    // res.writeHead(500, {'Content-Type': 'application/json'})
     //res.end(JSON.stringify({ message: 'Server Error'}));
+    
+    try{
+        //Check if the request method is GET
+        if (req.method === 'GET'){
+            if (req.url === '/'){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('<h1>Homepage</h1>');
+            } else if (req.url === '/about'){
+                res.writeHead(200, {'Content-Type' : 'text/html'});
+                res.end('<h1>About</h1>');
+            } else{
+                res.writeHead(404, {'Content-Type' : 'text/html'});
+                res.end(`<h1>Not Found</h1>`);
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`<h1> Hello World </h1>`);
+    } catch (error){
+        res.writeHead(500, {'Content-Type' : 'text/plain'});
+        res.end('Server Error');
+
+    }
+
 });
 
 
