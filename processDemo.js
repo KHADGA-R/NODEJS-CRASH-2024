82253c783566cc54f3b6a595cb521e24c06133c1
KHADGA-R <parajuli.kr@gmail.com>
2025-05-30 14:19:20 -0700

    adding process module

---
+++ processDemo.js
@@ -0,0 +1,18 @@
+
+// argv
+console.log(process.argv);
+
+// process.env
+
+//console.log(process.env);
+
+//pid
+console.log(process.pid);
+
+//title
+
+console.log(process.title);
+
+// memoryUsage()
+
+console.log(process.memoryUsage());
