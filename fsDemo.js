59dffeb2b0f0f5ad464326a348024eac4b9ff60b
KHADGA-R <parajuli.kr@gmail.com>
2025-05-29 20:39:52 -0700

    async-await version to read file

--- fsDemo.js
+++ fsDemo.js
@@ -13,6 +13,19 @@
 // console.log(data);
 
 //readFile()  - promise version
-readFile('./test.txt', 'utf8')
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+// readFile('./test.txt', 'utf8')
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+
+// readFile() - async/await version
+
+const readFileContent = async () => {
+    try{
+    const data = await readFile('./test.txt', 'utf8');
+    console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+readFileContent();
