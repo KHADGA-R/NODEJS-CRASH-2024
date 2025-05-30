a0d3d83cd83383f519fe678c0b20e53a3f9b1af3
KHADGA-R <parajuli.kr@gmail.com>
2025-05-29 21:48:37 -0700

    Os module

---
+++ osDemo.js
@@ -0,0 +1,18 @@
+import os from 'os';
+
+//userInfo()
+
+console.log(os.userInfo());
+
+
+//totalmem()
+
+console.log(os.totalmem());
+
+//freemem()
+
+console.log(os.freemem());
+
+//cpus()
+
+console.log(os.cpus());
