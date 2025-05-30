6c299125ffeb1b19e010761d32999179429d71ff
KHADGA-R <parajuli.kr@gmail.com>
2025-05-29 20:05:40 -0700

    file system sync, async and promise verion

--- fsDemo.js
+++ fsDemo.js
@@ -1,13 +1,18 @@
-import fs from 'fs';
+ import { readFile } from 'fs/promises';
 
 //readFile() - callback
-fs.readFile('./test.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
 
 
-// readFileSync() - synchronous version
+// // readFileSync() - synchronous version
 
-const data = fs.readFileSync('./test.txt', 'utf8');
-console.log(data);
+// const data = fs.readFileSync('./test.txt', 'utf8');
+// console.log(data);
+
+//readFile()  - promise version
+readFile('./test.txt', 'utf8')
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
